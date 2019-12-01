3673acdf1e023d0baa6cbac3831c55ab106fd321
Theo Sandell <theo01sandell@gmail.com>
2019-12-01 01:00:18 +0100

    Completed most of add layout

--- Java/app/src/main/java/com/example/theo/todoappjava/AddActivity.java
+++ Java/app/src/main/java/com/example/theo/todoappjava/AddActivity.java
@@ -1,31 +1,69 @@
 package com.example.theo.todoappjava;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.CheckBox;
 import android.widget.DatePicker;
+import android.widget.ScrollView;
 
 import java.util.Objects;
 
 public class AddActivity extends AppCompatActivity {
+
+    private static final String TAG = "AddActivity";
+
+    ActionBar ab;
+    DatePicker datePicker;
+    Toolbar toolbar;
+    CheckBox noDeadlineCheckbox;
+    ScrollView sV;
+    FloatingActionButton saveBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        saveBtn = (FloatingActionButton) findViewById(R.id.save_btn);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        ActionBar ab = getSupportActionBar();
+        ab = getSupportActionBar();
         Objects.requireNonNull(ab).setDisplayShowTitleEnabled(false);
 
         ab.setDisplayHomeAsUpEnabled(true);
 
-        DatePicker datePicker = (DatePicker) findViewById(R.id.deadline_input);
+        datePicker = (DatePicker) findViewById(R.id.deadline_input);
         datePicker.setMinDate(System.currentTimeMillis() - 1000);
+
+        noDeadlineCheckbox = (CheckBox) findViewById(R.id.no_deadline_checkbox);
+
+        /*noDeadlineCheckbox.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(((CheckBox) v).isChecked()){
+                    Log.d(TAG, "onClick: checkbox is checked");
+                    datePicker.setClickable(false);
+                    datePicker.setAlpha(0.5f);
+                    datePicker.setEnabled(false);
+                }else{
+                    Log.d(TAG, "onClick: Checkbox is NOT checked");
+                    datePicker.setClickable(true);
+                    datePicker.setAlpha(1.0f);
+                    datePicker.setEnabled(true);
+                }
+            }
+        });*/
+
+        sV = (ScrollView) findViewById(R.id.scrollView);
+        sV.scrollTo(0, sV.getBottom());
     }
 
     @Override
